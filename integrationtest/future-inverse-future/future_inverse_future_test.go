a1994daff176c34dfe9d630fbf12f75a960fbc51
Game <0xchiefalchemist@gmail.com>
2024-02-26 10:11:04 +0200

    Update imports

--- integrationtest/future-inverse-future/future_inverse_future_test.go
+++ integrationtest/future-inverse-future/future_inverse_future_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hirokisan/bybit/v2"
-	"github.com/hirokisan/bybit/v2/integrationtest/testhelper"
+	"github.com/Chief-Alchemist/bybit/v2"
+	"github.com/Chief-Alchemist/bybit/v2/integrationtest/testhelper"
 	"github.com/stretchr/testify/require"
 )
 
